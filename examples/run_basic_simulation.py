d6c899f7e474fd41707cad969a74118b84fc1763
LouisaPawusch <louisa.pawusch@googlemail.com>
2026-04-02 13:28:34 -0400

    tiny prettifications of example run

--- examples/run_basic_simulation.py
+++ examples/run_basic_simulation.py
@@ -14,15 +14,12 @@ def initial_temperature(X, Y):
     return np.zeros_like(X)
 
 def main():
-    problem = HeatTransportProblem(Lx=1.0, Ly=1.0, Nx=51, Ny=51, alpha=1e-3, R_th=2.0, rho_c_eff=1.0, vx=0.1, vy=0.0, initial_condition_fn=initial_temperature, 
-        bc_left_type="dirichlet", bc_left_value=1.0, bc_right_type="neumann", bc_right_value=0.0,
-        bc_bottom_type="neumann", bc_bottom_value=0.0, bc_top_type="neumann", bc_top_value=0.0,)
+    problem = HeatTransportProblem(Lx=1.0, Ly=1.0, Nx=51, Ny=51, alpha=1e-3, R_th=2.0, rho_c_eff=1.0, vx=0.1, vy=0.0, 
+                                   initial_condition_fn=initial_temperature, bc_left_type="dirichlet", bc_left_value=1.0, 
+                                   bc_right_type="neumann", bc_right_value=0.0, bc_bottom_type="neumann", bc_bottom_value=0.0, 
+                                   bc_top_type="neumann", bc_top_value=0.0)
 
-    times, all_T = run_simulation( problem, t_final=1.0, dt=1e-3, save_every=100, advection_scheme="upwind")
-
-    print("Simulation completed. all_T.shape:", all_T[-1].shape)
-    print("Final temperature distribution:")
-    print(all_T[-1])
+    times, all_T = run_simulation(problem, t_final=10.0, dt=1e-3, save_every=1, advection_scheme="central")
 
     plot_initial_and_final(problem, times, all_T, save_path="results/plots/basic_simulation_init_and_final.png", cmap="inferno")
 
