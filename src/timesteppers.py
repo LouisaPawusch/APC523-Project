3fe2c9b01a35752d8bf7e1ab933991b336ee31a3
LouisaPawusch <louisa.pawusch@googlemail.com>
2026-04-02 13:27:11 -0400

    outsorcing first timestepping method (FE) into the timesteppers.py

--- src/timesteppers.py
+++ src/timesteppers.py
@@ -1,1 +1,10 @@
 #RK4, Backward Euler, and Forward Euler time-stepping methods
+
+from .problem import HeatTransportProblem
+
+def forward_euler_step(T, problem, operators, t, dt):
+
+    rhs = problem.compute_rhs(T, operators, t)
+    T_new = T + dt * rhs
+
+    return T_new
